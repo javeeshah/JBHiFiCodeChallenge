66eece2443b60bcb5579149bb9761366bb9ab922
Jawad <javee_shah@hotmail.com>
2022-11-13 16:26:19 +1100

    Master > Updated the Rate Limit value to 5 in an hour

--- CurrentWeatherApi/Program.cs
+++ CurrentWeatherApi/Program.cs
@@ -34,7 +34,7 @@ builder.Services.Configure<ClientRateLimitOptions>(options =>
             {
                 Endpoint = "*",
                 Period = "60m",
-                Limit = 2
+                Limit = 5
             }
         };
 });
